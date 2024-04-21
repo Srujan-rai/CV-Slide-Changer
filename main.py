2e37fd5462afe5946f9e5726a1f3dd3f32859da6
Srujan-rai <raisrujan@gmail.com>
2024-04-21 10:59:59 +0530

    code push

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-# TechVidvan hand Gesture Recognizer
 
-# import necessary packages
 
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+import pyautogui 
+import time
 
-# initialize mediapipe
+
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
-# Load the gesture recognizer model
+
 model = load_model('mp_hand_gesture')
 
-# Load class names
+
 f = open('gesture.names', 'r')
 classNames = f.read().split('\n')
 f.close()
 print(classNames)
 
 
-# Initialize the webcam
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -67,7 +67,13 @@ while True:
     cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (0,0,255), 2, cv2.LINE_AA)
 
-    # Show the final output
+    if className=="fist":
+        pyautogui.press('right')
+        time.sleep(3)
+    elif  className=="smile":
+        pyautogui.press('left')
+        time.sleep(3)
+
     cv2.imshow("Output", frame) 
 
     if cv2.waitKey(1) == ord('q'):
